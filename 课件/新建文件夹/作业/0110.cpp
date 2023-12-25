76902d3aa993795d7cf7100ae52e0059e4cd48a2
wurldwen <1287356875@qq.com>
2023-12-25 19:04:59 +0800

    update

---
+++ 课件/新建文件夹/作业/0110.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstdlib>
+#define pi  3.1415
+
+using namespace std;
+
+    int main(void){
+    	float r, S;
+    	
+    	cin >> r >> S;
+    	
+		
+    	
+		S = pi * r * r;//面积公式
+		
+		cout<<"圆的面积是： "<<S<<endl;
+		
+		system("pause");
+		
+		return 0; 
+	}
+    
